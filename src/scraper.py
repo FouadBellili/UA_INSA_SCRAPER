179a0c95c15f8c6598668e2b568c3566c56762c0
fbellili <fbellili@insa-rouen.fr>
2026-02-24 16:07:41 +0000

    scraper optimization

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,6 @@
 import logging
 import pathlib
+import time
 
 from playwright.sync_api import sync_playwright
 
@@ -7,8 +8,9 @@ i=0
 BASE_DIR = pathlib.Path(__file__).parent.parent
 BRONZE_DIR = BASE_DIR / "data" / "bronze" / "ua_news"
 BRONZE_DIR.mkdir(parents=True, exist_ok=True)
+
 with sync_playwright() as p:
-    browser = p.chromium.launch()
+    browser = p.chromium.launch(headless=True)
     page = browser.new_page()
     try:
         page.goto("https://www.ua.pt/pt/noticias/3")
@@ -18,12 +20,18 @@ with sync_playwright() as p:
 
 
     while page.get_by_role("button", name="Carregar mais").is_visible():
-        i=i+1
+        i += 1
         html_raw = page.content()
         file_path = BRONZE_DIR / f"page_{i}.html"
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(html_raw)
-            logging.info(f"Page {i} written to {file_path}")
-        page.get_by_role("button", name="Carregar mais").click()
-        page.wait_for_load_state("networkidle")
+        logging.info(f"Page {i} written to {file_path}")
+        try:
+            page.get_by_role("button", name="Carregar mais").click(timeout=5000)
+            page.wait_for_load_state("networkidle")
+            time.sleep(1)
+        except Exception as e:
+            print(f"Error clicking 'Carregar mais' button: {e}")
+            break
+        
     browser.close()
